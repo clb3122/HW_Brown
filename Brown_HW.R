75e0e5c06fc8eb1c3d0dd976ac058bc6ca6f9371
clb3122 <clb3122@outlook.com>
2025-02-13 18:40:05 -0500

    Print mean and SD

--- Brown_HW.R
+++ Brown_HW.R
@@ -1,3 +1,4 @@
 random_numbers <- rnorm(100, 25, 1)
 mean_value <- mean(random_numbers)
 sd_value <- sd(random_numbers)
+print(paste(The Mean is, mean_value, and the Standard Deviation is, sd_value))
